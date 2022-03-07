5f8762ee8b7b4c0dc86a01f4ecb3258c613e3d3d
Winston White <whitew2015@fau.edu>
2022-03-07 14:21:32 -0500

    Added and removed certain menubar features

--- editor/js/Menubar.js
+++ editor/js/Menubar.js
@@ -1,13 +1,18 @@
 import { UIPanel } from './libs/ui.js';
 
-import { MenubarAdd } from './Menubar.Add.js';
+
+//remove
+//import { MenubarAdd } from './Menubar.Add.js';
 import { MenubarEdit } from './Menubar.Edit.js';
 import { MenubarFile } from './Menubar.File.js';
 import { MenubarExamples } from './Menubar.Examples.js';
 import { MenubarView } from './Menubar.View.js';
 import { MenubarHelp } from './Menubar.Help.js';
-import { MenubarPlay } from './Menubar.Play.js';
+//remove
+//import { MenubarPlay } from './Menubar.Play.js';
 import { MenubarStatus } from './Menubar.Status.js';
+//keep; crashes the editor if uncommented
+/*import { MenubarPublish } from './Menubar.Publish.js';*/
 
 function Menubar( editor ) {
 
@@ -16,10 +21,13 @@ function Menubar( editor ) {
 
 	container.add( new MenubarFile( editor ) );
 	container.add( new MenubarEdit( editor ) );
-	container.add( new MenubarAdd( editor ) );
-	container.add( new MenubarPlay( editor ) );
+	//remove
+	//container.add( new MenubarAdd( editor ) );
+	//container.add( new MenubarPlay( editor ) );
 	container.add( new MenubarExamples( editor ) );
 	container.add( new MenubarView( editor ) );
+	//keep
+	//container.add( new MenubarPublish( editor ) );
 	container.add( new MenubarHelp( editor ) );
 
 	container.add( new MenubarStatus( editor ) );
